bc594124faf8113f65ee91a569b017d09d25c46a
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2017-03-21 22:13:14 -0400

    utility function for writing files

--- src/Text/HTML/AudioDataURIs.hs
+++ src/Text/HTML/AudioDataURIs.hs
@@ -5,9 +5,14 @@ import qualified Data.ByteString.Lazy as BL
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.Text.Lazy as T
 import Data.Text.Lazy (Text)
+import System.FilePath (joinPath)
 
-convert :: String -> FilePath -> Text -> (Text, [(FilePath, ByteString)])
-convert uri outDir html = ("", [])
+convert :: String -> Text -> (Text, [(FilePath, ByteString)])
+convert uri html = ("", [])
+
+writeToFiles :: FilePath -> [(FilePath, ByteString)] -> IO ()
+writeToFiles outDir = mapM_ f
+  where f (fname, value) = BL.writeFile (joinPath [outDir, fname]) value
 
 cliMain :: IO ()
 cliMain = return ()
--- test/Text/HTML/AudioDataURIsSpec.hs
+++ test/Text/HTML/AudioDataURIsSpec.hs
@@ -8,6 +8,10 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text.Lazy as T
 import Text.HTML.AudioDataURIs
 
+import System.FilePath (joinPath)
+import System.Directory (listDirectory)
+import System.IO.Temp (withSystemTempDirectory)
+
 exampleHTML :: T.Text
 exampleHTML = T.unlines
   [ "<audio controls=\"controls\" >"
@@ -24,12 +28,25 @@ exampleConvertedHTML = T.unlines
   , "</audio>"
   ]
 
+writeToTemp :: [(FilePath, BL.ByteString)] -> IO [(FilePath, BL.ByteString)]
+writeToTemp inp =
+  withSystemTempDirectory "out_files" $ \dirname -> do
+    writeToFiles dirname inp
+    written <- listDirectory dirname
+    mapM (\x -> (,) <$> pure x <*> BL.readFile (joinPath [dirname, x])) written
+
+
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
   describe "convert" $ do
-    context "when given a uri prefix (ending in /), an out directory, and an HTML string" $ do
+    context "when given a uri prefix (ending in /) and an HTML string" $ do
       it "returns a new HTML string and a list of (FilePath, ByteString) tuples" $ do
-        convert "/static/out/" "_site/out/" exampleHTML `shouldBe` (exampleConvertedHTML, [("out1.wav", BL.empty)])
+        convert "/static/out/" exampleHTML `shouldBe` (exampleConvertedHTML, [("out1.wav", BL.empty)])
+  describe "writeToFiles" $ do
+    context "when given an out directory" $
+      it "writes the (FilePath, ByteString) tuples to files in the directory" $ do
+      let xs = [("out1.txt", "Test."), ("out2.txt", "Test2.")]
+      writeToTemp xs `shouldReturn` xs
